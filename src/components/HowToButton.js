e2c32ed590886e05b87c528745765782dd3428a5
JenColfordNL <jennifer.colford@keyin.com>
2023-07-04 10:47:19 -0230

    change to alert

--- src/components/HowToButton.js
+++ src/components/HowToButton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function HowToButton() {
   const handleClick = () => {
     alert(
-      "You have six attempts to guess the Newfoundlandle. If a sqaure turns pink, the letter is in the Newfoundlandle but not in the right position.  If the square turns green, you guessed the right letter in the right position on the board. If the square turns grey, the letter you guessed is not in the Newfoundlandle"
+      "You have six attempts to guess the Newfoundland word. Each guess must be a valid 5-letter word. If a sqaure turns pink, the letter is in the Newfoundland word but in the wrong position.  If the square turns green, you guessed the right letter in the right position. If the square turns grey, the letter you guessed is not in the Newfoundland word"
     );
   };
 
